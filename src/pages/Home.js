0067c0685cb5b542b186bf584588a8f602a8c5f0
Aiman shariff G <aimanshariff19@gmail.com>
2026-03-03 20:24:57 +0530

    Fix import statement for certiq-logo.svg

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
-import logo from "../assets/certiq-logo.svg":
+import logo from "../assets/certiq-logo.svg";
 
 function Home() {
 
